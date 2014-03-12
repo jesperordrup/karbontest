2c425c697c88a9abad8162c83d936032bac1deb7
Jesper Ordrup <jesper@jesper.com>
2014-03-12 16:48:11 +0100

    Renamed fiskfisk to Home

--- Karbon test 1/Models/Home.cs
+++ Karbon test 1/Models/Home.cs
@@ -6,7 +6,7 @@ using System.Web;
 
 namespace Karbon_test_1.Models
 {
-    public class FiskFisk : Karbon.Cms.Core.Models.Content
+    public class Home : Karbon.Cms.Core.Models.Content
     {
         public string MitFelt { get; set; }
         public string Content { get; set; }
